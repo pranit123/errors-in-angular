4eaa927ca2789d570a8c3b2242b776c546623fc3
pranit123 <53820823+pranit123@users.noreply.github.com>
2019-09-11 21:37:48 +0530

    minor error

---
+++ form-td-start/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData:{
+        username: suggestedName
+      }
+    });
+  }
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+}
